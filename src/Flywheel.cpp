9fb1269a02f6c1c49174d7c6423cb2a7e510b8dc
BuildGinger11 <56709573+BuildGinger11@users.noreply.github.com>
2022-06-23 11:51:33 -0700

    flywheel updated

--- src/Flywheel.cpp
+++ src/Flywheel.cpp
@@ -4,6 +4,9 @@
 pros::Motor Flywheel(9);
 pros::Motor Flywheel2(10, true);
 
+int speeds[2] = {80, 127};
+bool speed = false;
+
 void flywheel (int power)
 {
   Flywheel.move(power);
@@ -16,11 +19,15 @@ void Flywheel_Control (void*)
  {
    if(master.get_digital(DIGITAL_R2))
    {
-     flywheel(127);
+     speed = !speed;
+   }
+   if(speed)
+   {
+     flywheel(speeds[0]);
    }
    else
    {
-     flywheel(0);
+     flywheel(speeds[1]);
    }
   pros::delay(20);
  }
